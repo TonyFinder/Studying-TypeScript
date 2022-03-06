9f0ffd439d129eccd79dd5ce2166b1918f8d95f8
Anton <antonrozdobudko@gmail.com>
2022-03-06 14:15:29 +1000

    Lesson 14. Promises 2nd

--- src/14/14.tsx
+++ src/14/14.tsx
@@ -56,11 +56,207 @@ console.log('14 lesson. Promise')
     return promise
 }*/
 
-const getRandom = () => new Promise((res, rej) => setTimeout(() => res(Math.random()), 2000))
+/*const getRandom = () => new Promise((res, rej) => setTimeout(() => res(Math.random()), 2000))
 
 getRandom().then(res => console.log(res))
-getRandom().then(res => console.log(res))
+getRandom().then(res => console.log(res))*/
+
+// #4. Примеры с Local storage
+
+//1. Синхронный код
+
+/*const localActions = {
+    save(data: string) {
+        try {
+            localStorage.setItem('init-info', data)
+        } catch(err) {
+            return false
+        }
+        return true
+    }
+}
+localActions.save("Anton") ? console.log("Saved") : console.warn("Not saved")*/
+
+//2. Синхронный код + Асинхронный
+
+/*const localActions = {
+    save(data: string) {
+        try {
+            localStorage.setItem('init-info', data)
+        } catch (err) {
+            return false
+        }
+        return true
+    },
+    saveAsync(data: string) {
+        return new Promise((resolve, reject) => {
+            try {
+                localStorage.setItem('init-info', data)
+                resolve(true)
+            } catch (err) {
+                reject(err)
+            }
+        })
+    }
+}
+
+localActions.saveAsync("Anton using async")
+    .then(response => console.log("Saved", response))
+    .catch(err => console.warn("Not saved", err))*/
+
+//3. Синхронный код + Асинхронный c async / await
+
+/*
+const localActions = {
+    save(data: string) {
+        try {
+            localStorage.setItem('init-info', data)
+        } catch (err) {
+            return false
+        }
+        return true
+    },
+    saveAsync(data: string) {
+        return new Promise((resolve, reject) => {
+            try {
+                localStorage.setItem('init-info', data)
+                resolve(true)
+            } catch (err) {
+                reject(err)
+            }
+        })
+    }
+}
+
+const run = async () => {
+    await localActions.saveAsync("Anton with async")
+    console.log("Saved again")
+}
+run()
+*/
+
+//4. Синхронный код + Асинхронный c async / await. Добавляем чтение данных
+
+/*const localActions = {
+    save(data: string) {
+        try {
+            localStorage.setItem('init-info', data)
+        } catch (err) {
+            return false
+        }
+        return true
+    },
+    saveAsync(data: string) {
+        return new Promise((resolve, reject) => {
+            try {
+                localStorage.setItem('init-info', data)
+                resolve(true)
+            } catch (err) {
+                reject(err)
+            }
+        })
+    },
+    readAsync() {
+        return new Promise(((resolve, reject) => {
+            localStorage.getItem('init-info')
+                ? resolve(localStorage.getItem('init-info'))
+                : resolve(null)
+        }))
+    }
+}
+
+const run = async () => {
+    await localActions.saveAsync('Anton with async')
+    console.log('Saved again')
+    const data = await localActions.readAsync()
+    console.log(data)
+}
+run()*/
+
+//5. Синхронный код + Асинхронный c async / await. Таймер, чтобы показывал через секунду 1,2,3..
+
+//Мой код
+/*const num = 1
+
+const localActions = {
+    timer(num: number) {
+        if (num < 6) {
+            setTimeout(() => {
+                console.log(num)
+                num++
+                run(num)
+            }, 1000)
+        } else return
+    }
+}
+
+const run = (num: number) => {
+    localActions.timer(num)
+}
+run(num)*/
+
+//С урока
+
+/*const wait = (ms: number) => {
+    return new Promise(resolve => {
+        setTimeout(()=>{
+            resolve(true)
+        }, ms)
+    })
+}
+
+const run = async () => {
+    await wait(1000)
+    console.log(1)
+    await wait(1000)
+    console.log(2)
+    await wait(1000)
+    console.log(3)
+}
+run()*/
+
+//6. Задачка
+
+const api = {
+    save() {
+
+    },
+    read() {
+        return Promise.resolve({name: "Anton's read message"})
+    }
+}
+
+const run = async () => {
+    await api.save()
+    console.log("I was saved")
+    let reading = await api.read()
+    console.log(reading)
+}
+run()
 
 
 
-export default ()=>{}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default () => {
+}
