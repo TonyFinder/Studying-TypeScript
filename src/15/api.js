4358a69deb638892cf2d47923b6f6fdc8b8c33d3
Anton <antonrozdobudko@gmail.com>
2022-03-25 21:33:21 +1000

    Lesson 15. Promises 3rd.

---
+++ src/15/api.js
@@ -0,0 +1,13 @@
+
+const api = {
+    sendStudentsCountToItKamasutra(studentsCount) {
+        return axios.post("https://it-kamasutra.com/students-need", 
+        { count: studentsCount });
+    },
+    getVacanciesCountFromMicrosoft() {
+        return axios.get("https://microsoft.com");
+    },
+    getVacanciesCountFromGoogle() {
+        return axios.get("https://google.com");
+    }
+}
